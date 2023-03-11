c1acb77300980e731d2d4840036d1f448e97653f
Steve McDaniel <steve@klumzy.com>
2023-03-11 16:58:40 -0700

    update build

---
+++ cmd/gorbd/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/nibbleshift/gorb/ent"
+	"github.com/nibbleshift/gorb/ent/migrate"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/nibbleshift/gorb"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	// Create ent.Client and run the schema migration.
+	client, err := ent.Open("postgres", "user=postgres password=example dbname=gorb sslmode=disable")
+
+	if err != nil {
+		log.Fatal("opening ent client", err)
+	}
+	if err := client.Schema.Create(
+		context.Background(),
+		migrate.WithGlobalUniqueID(true),
+	); err != nil {
+		log.Fatal("opening ent client", err)
+	}
+
+	// Configure the server and start listening on :8081.
+	srv := handler.NewDefaultServer(gorb.NewSchema(client))
+	http.Handle("/",
+		playground.Handler("gorb", "/query"),
+	)
+	http.Handle("/query", srv)
+	log.Println("listening on :8081")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		log.Fatal("http server terminated", err)
+	}
+}
--- resolver.go
+++ resolver.go
@@ -1,7 +1,20 @@
 package gorb
 
+import (
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/nibbleshift/gorb/ent"
+)
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+// Resolver is the resolver root.
+type Resolver struct{ client *ent.Client }
+
+// NewSchema creates a graphql executable schema.
+func NewSchema(client *ent.Client) graphql.ExecutableSchema {
+	return NewExecutableSchema(Config{
+		Resolvers: &Resolver{client},
+	})
+}
