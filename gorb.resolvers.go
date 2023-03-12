7e8c5848197d334e375b7c9c728ed56d59ac85f5
Steve McDaniel <steve@klumzy.com>
2023-03-11 17:02:39 -0700

    add resolver

---
+++ gorb.resolvers.go
@@ -0,0 +1,20 @@
+package gorb
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/nibbleshift/gorb/ent"
+)
+
+func (r *mutationResolver) CreateBenchmark(ctx context.Context, input ent.CreateBenchInput) (*ent.Bench, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Mutation returns MutationResolver implementation.
+func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
+
+type mutationResolver struct{ *Resolver }
