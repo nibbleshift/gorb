7682f1eeaceb30181a77bf6625f9f5037e524da2
Steve McDaniel <steve@klumzy.com>
2023-03-15 20:36:16 -0600

    Added UI benchmarks page

--- ui/src/plugins/index.ts
+++ ui/src/plugins/index.ts
@@ -11,10 +11,34 @@ import router from '../router'
 
 // Types
 import type { App } from 'vue'
+import ApolloClient from 'apollo-boost'
+import { createApolloProvider } from '@vue/apollo-option'
+import Vue from 'vue'
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
+
+
+const httpLink = new HttpLink({
+  // You should use an absolute URL here
+  uri: 'http://localhost:8081/query',
+})
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+})
+
+// Create a provider
+const apolloProvider = createApolloProvider({
+  defaultClient: apolloClient,
+})
 
 export function registerPlugins (app: App) {
   loadFonts()
   app
     .use(vuetify)
     .use(router)
+    .use(apolloProvider)
 }
--- ui/src/views/Benchmarks.vue
+++ ui/src/views/Benchmarks.vue
@@ -1,6 +1,74 @@
 <template>
-  Benchmarks
+    <v-container  fluid>
+      <v-responsive class="pa-3 align-center  ">
+        <div class="text-h4 pb-3">Benchmarks</div>
+        <v-card elevation="1">
+          <v-table>
+            <thead>
+              <tr>
+                <th class="text-left">
+                  Package 
+                </th>
+                <th class="text-left">
+                  OS
+                </th>
+                <th class="text-left">
+                  Arch
+                </th>
+                <th class="text-left">
+                  CPU
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in benches.edges" :key="item.id">
+                <td>{{ item.node.package}}</td>
+                <td>{{ item.node.os }}</td>
+                <td>{{ item.node.arch }}</td>
+                <td>{{ item.node.cpu}}</td>
+              </tr>
+            </tbody>
+          </v-table>
+        </v-card>
+      </v-responsive>
+    </v-container>
 </template>
 
-<script lang="ts" setup>
+<script>
+import { gql } from "apollo-boost";
+
+const FETCH_BENCHMARKS = gql`
+        query benches {
+          benches {
+            edges {
+              node {
+                id
+                os
+                arch
+                cpu
+                package
+                benchResult {
+                  name
+                  allocsPerOp
+                  allocedBytesPerOp
+                  mbPerS
+                  n
+                  nsPerOp
+                }
+              }
+            }
+          }
+        }`;
+export default {
+  data() {
+    return {
+      benches: []
+    }
+  },
+  apollo: {
+    benches: {
+      query: FETCH_BENCHMARKS,
+    }
+  }
+};
 </script>
