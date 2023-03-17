cf949e757536a7f0d44206414eee14547cc3788e
Steve McDaniel <steve@klumzy.com>
2023-03-16 21:50:36 -0600

    added dashboard mockup

--- ui/src/views/Dashboard.vue
+++ ui/src/views/Dashboard.vue
@@ -1,85 +1,91 @@
 <template>
-    <v-container  fluid>
-      <v-row no-gutters>
-        <v-card elevation="2" class="pa-5">
-          <v-card-title>Latest Results</v-card-title>
-          <v-timeline side="end" align="start">
-            <v-timeline-item
-              dot-color="pink"
-              size="small"
-            >
-              <div class="d-flex">
-                <strong class="me-3">3/14/2023 17:01</strong>
-                <div>
-                  <strong>Test</strong>
-                  <div class="text-caption">
-                    Package github.com/benthosdev/benthos
+    <v-container  fluid class="fill-height">
+      <v-row no-gutters class="fill-height">
+        <v-col>
+        </v-col>
+        <v-col>
+        </v-col>
+        <v-col order="last">
+         <v-card elevation="1" class="fill-height">
+           <v-card-title>Latest Results</v-card-title>
+            <v-timeline side="end" align="start">
+              <v-timeline-item
+                dot-color="pink"
+                size="small"
+              >
+                <div class="d-flex">
+                  <strong class="me-3">3/14/2023 17:01</strong>
+                  <div>
+                    <strong>Test</strong>
+                    <div class="text-caption">
+                      Package github.com/benthosdev/benthos
+                    </div>
                   </div>
                 </div>
-              </div>
-            </v-timeline-item>
-        
-            <v-timeline-item
-              dot-color="teal-lighten-3"
-              size="small"
-            >
-              <div class="d-flex">
-                <strong class="me-4">3/15/2023 11:34</strong>
-                <div>
-                  <strong>Benchmark</strong>
-                  <div class="text-caption mb-2">
-                    Package github.com/benthosdev/benthos
+              </v-timeline-item>
+          
+              <v-timeline-item
+                dot-color="teal-lighten-3"
+                size="small"
+              >
+                <div class="d-flex">
+                  <strong class="me-4">3/15/2023 11:34</strong>
+                  <div>
+                    <strong>Benchmark</strong>
+                    <div class="text-caption mb-2">
+                      Package github.com/benthosdev/benthos
+                    </div>
                   </div>
                 </div>
-              </div>
-            </v-timeline-item>
-
-            <v-timeline-item
-              dot-color="teal-lighten-3"
-              size="small"
-            >
-              <div class="d-flex">
-                <strong class="me-4">3/15/2023 11:42</strong>
-                <div>
-                  <strong>Benchmark</strong>
-                  <div class="text-caption mb-2">
-                    Package github.com/benthosdev/benthos
+              </v-timeline-item>
+  
+              <v-timeline-item
+                dot-color="teal-lighten-3"
+                size="small"
+              >
+                <div class="d-flex">
+                  <strong class="me-4">3/15/2023 11:42</strong>
+                  <div>
+                    <strong>Benchmark</strong>
+                    <div class="text-caption mb-2">
+                      Package github.com/benthosdev/benthos
+                    </div>
                   </div>
                 </div>
-              </div>
-            </v-timeline-item>
-
-            <v-timeline-item
-              dot-color="teal-lighten-3"
-              size="small"
-            >
-              <div class="d-flex">
-                <strong class="me-4">3/15/2023 11:46</strong>
-                <div>
-                  <strong>Benchmark</strong>
-                  <div class="text-caption mb-2">
-                    Package github.com/benthosdev/benthos
+              </v-timeline-item>
+  
+              <v-timeline-item
+                dot-color="teal-lighten-3"
+                size="small"
+              >
+                <div class="d-flex">
+                  <strong class="me-4">3/15/2023 11:46</strong>
+                  <div>
+                    <strong>Benchmark</strong>
+                    <div class="text-caption mb-2">
+                      Package github.com/benthosdev/benthos
+                    </div>
                   </div>
                 </div>
-              </div>
-            </v-timeline-item>
-        
-            <v-timeline-item
-              dot-color="pink"
-              size="small"
-            >
-              <div class="d-flex">
-                <strong class="me-4">3/17/2023 19:21</strong>
-                <div>
-                  <strong>Test</strong>
-                  <div class="text-caption">
-                    Package github.com/benthosdev/benthos
+              </v-timeline-item>
+          
+              <v-timeline-item
+                dot-color="pink"
+                size="small"
+              >
+                <div class="d-flex">
+                  <strong class="me-4">3/17/2023 19:21</strong>
+                  <div>
+                    <strong>Test</strong>
+                    <div class="text-caption">
+                      Package github.com/benthosdev/benthos
+                    </div>
                   </div>
                 </div>
-              </div>
-            </v-timeline-item>
-          </v-timeline>
-	</v-card>
+              </v-timeline-item>
+            </v-timeline>
+	 </v-card>
+        </v-col>
       </v-row>
     </v-container>
 </template>
