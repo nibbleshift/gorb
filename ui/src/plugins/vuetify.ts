10c2c6ac78b999abfad87ca0d6c6968815b2ed42
Steve McDaniel <steve@klumzy.com>
2023-03-16 20:55:53 -0600

    updates to tables

--- ui/src/plugins/vuetify.ts
+++ ui/src/plugins/vuetify.ts
@@ -10,9 +10,19 @@ import 'vuetify/styles'
 
 // Composables
 import { createVuetify } from 'vuetify'
+import { VDataTable } from 'vuetify/labs/VDataTable'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  components: {
+    VDataTable,
+  },
+  defaults: {
+    VDataTable: {
+      fixedHeader: true,
+      noDataText: 'Results not found',
+    },
+  },
   theme: {
     themes: {
       light: {
--- ui/src/views/Benchmarks.vue
+++ ui/src/views/Benchmarks.vue
@@ -2,34 +2,50 @@
     <v-container  fluid>
       <v-responsive class="pa-3 align-center  ">
         <div class="text-h4 pb-3">Benchmarks</div>
-        <v-card elevation="1">
-          <v-table>
-            <thead>
+        <v-card elevation="1" class="mb-5">
+	  <v-data-table
+            show-expand
+            :headers="headers" 
+            :items="benches.edges">
+	    <template v-slot:top>
+              <v-toolbar class="bg-deep-purple">
+                <v-toolbar-title>Benchmarks</v-toolbar-title>
+              </v-toolbar>
+            </template>
+            <template v-slot:expanded-row="{ columns, item }">
               <tr>
-                <th class="text-left">
-                  Package 
-                </th>
-                <th class="text-left">
-                  OS
-                </th>
-                <th class="text-left">
-                  Arch
-                </th>
-                <th class="text-left">
-                  CPU
-                </th>
+                <td :colspan="columns.length">
+                  <v-table>
+                    <thead>
+                      <tr>
+                        <th>Name</th>
+                        <th>Iterations</th>
+                        <th>ns/op</th>
+                        <th>alloced bytes/sec</th>
+                        <th>allocs per op</th>
+                        <th>MB/sec</th>
+                        <th>Measured</th>
+                        <th>Ord</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr v-for="r in item.value.node.benchResult" :key="Name">
+                        <td>{{r.name}}</td>
+                        <td>{{r.n}}</td>
+                        <td>{{r.nsPerOp}}</td>
+                        <td>{{r.allocedBytesPerOp}}</td>
+                        <td>{{r.allocsPerOp}}</td>
+                        <td>{{r.mbPerS}}</td>
+                        <td>{{r.measured}}</td>
+                        <td>{{r.ord}}</td>
+                      </tr>
+                    </tbody>
+                  </v-table>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              <tr v-for="item in benches.edges" :key="item.id">
-                <td>{{ item.node.package}}</td>
-                <td>{{ item.node.os }}</td>
-                <td>{{ item.node.arch }}</td>
-                <td>{{ item.node.cpu}}</td>
-              </tr>
-            </tbody>
-          </v-table>
-        </v-card>
+            </template>
+	  </v-data-table>
+	</v-card>
       </v-responsive>
     </v-container>
 </template>
@@ -62,7 +78,25 @@ const FETCH_BENCHMARKS = gql`
 export default {
   data() {
     return {
-      benches: []
+      benches: [],
+      headers: [
+        {
+	  title: "Package",
+	  key: "node.package",
+	},
+        {
+	  title: "OS",
+	  key: "node.os",
+	},
+        {
+	  title: "Arch",
+	  key: "node.arch",
+	},
+        {
+	  title: "CPU",
+	  key: "node.cpu",
+	},
+      ]
     }
   },
   apollo: {
